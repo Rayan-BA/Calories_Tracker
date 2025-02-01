3b288410c07fba0608d83022242ec179c23d522f
RaychuW <rb.alhamdan@gmail.com>
2025-02-01 12:53:43 +0300

    meal + MealFood join table

---
+++ api/Models/Meal.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDbGenericRepository.Attributes;
+
+namespace api.Models;
+
+[CollectionName("meals")]
+public class Meal
+{
+  [BsonId]
+  [BsonElement("_id"), BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+  public string? Id { get; set; }
+  [BsonElement("name")]
+  public string? Name { get; set; }
+}
---
+++ api/Models/MealFood.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDbGenericRepository.Attributes;
+
+namespace api.Models;
+
+[CollectionName("mealFoods")]
+public class MealFood
+{
+  [BsonId]
+  [BsonElement("_id")]
+  public object? Id { get; set; }
+  [BsonElement("meal")]
+  public Meal? Meal { get; set; }
+  [BsonElement("food")]
+  public Food? Food { get; set; }
+}
