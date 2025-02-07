093c08ccf7e18092f75fa034f7186f511d83c7f7
RaychuW <rb.alhamdan@gmail.com>
2025-02-07 16:17:04 +0300

    shorter more elegant implmentation

--- api/Controllers/EntryController.cs
+++ api/Controllers/EntryController.cs
@@ -1,10 +1,10 @@
-﻿using api.DTOs.FoodEntry;
+using api.DTOs.FoodEntry;
 using api.DTOs.MealEntry;
 using api.Interfaces;
 using api.Models;
 using Mapster;
-using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace api.Controllers;
 
@@ -39,88 +39,115 @@ public class EntryController(IFoodEntryRepository foodEntryRepository, IMealEntr
     return Ok(entriesDTO);
   }
 
-  [HttpGet("food/{id}")]
-  public async Task<IActionResult> GetFoodEntry([FromRoute] string id)
+  [HttpGet("{type}/{id}")]
+  public async Task<IActionResult> GetEntry([FromRoute] string type, [FromRoute] string id)
   {
-    var foodEntry = await _foodEntryRepository.GetFoodEntry(id);
-
-    if (foodEntry == null)
-      return NotFound("Entry not found");
-
-    return Ok(foodEntry.Adapt<FoodEntryDTO>());
+    if (type == "food")
+    {
+      var foodEntry = await _foodEntryRepository.GetFoodEntry(id);
+      if (foodEntry == null)
+        return NotFound("Entry not found");
+      return Ok(foodEntry.Adapt<FoodEntryDTO>());
+    }
+    else if (type == "meal")
+    {
+      var mealEntry = await _mealEntryRepository.GetMealEntry(id);
+      if (mealEntry == null)
+        return NotFound("Entry not found");
+      return Ok(mealEntry.Adapt<MealEntryDTO>());
+    }
+    return BadRequest("Invalid entry type");
   }
 
-  [HttpGet("meal/{id}")]
-  public async Task<IActionResult> GetMealEntry([FromRoute] string id)
+  [HttpPost("{type}")]
+  public async Task<IActionResult> CreateEntry([FromRoute] string type, [FromBody] JsonElement entryDTO)
   {
-    var mealEntry = await _mealEntryRepository.GetMealEntry(id);
-
-    if (mealEntry == null)
-      return NotFound("Entry not found");
-
-    return Ok(mealEntry.Adapt<MealEntryDTO>());
+    if (type == "food")
+    {
+      var foodEntryDTO = new CreateFoodEntryDTO
+      {
+        FoodId = entryDTO.GetProperty("foodId").GetString(),
+        ServingSize = entryDTO.GetProperty("servingSize").GetInt32(),
+        ServingSizeUnit = entryDTO.GetProperty("servingSizeUnit").GetString(),
+        CreatedBy = entryDTO.GetProperty("createdBy").GetString(),
+      };
+      if (foodEntryDTO == null)
+        return BadRequest("Invalid data");
+      var foodEntry = await _foodEntryRepository.CreateFoodEntry(foodEntryDTO.Adapt<FoodEntry>());
+      return CreatedAtAction(nameof(GetEntry), new { type = "food", id = foodEntry.Id }, foodEntryDTO);
+    }
+    else if (type == "meal")
+    {
+      var mealEntryDTO = new CreateMealEntryDTO
+      {
+        MealId = entryDTO.GetProperty("mealId").GetString(),
+        CreatedBy = entryDTO.GetProperty("createdBy").GetString(),
+      };
+      if (mealEntryDTO == null)
+        return BadRequest("Invalid data");
+      var mealEntry = await _mealEntryRepository.CreateMealEntry(mealEntryDTO.Adapt<MealEntry>());
+      return CreatedAtAction(nameof(GetEntry), new { type = "meal", id = mealEntry.Id }, mealEntryDTO);
+    }
+    return BadRequest("Invalid entry type");
   }
 
-  [HttpPost("food")]
-  public async Task<IActionResult> CreateFoodEntry([FromBody] CreateFoodEntryDTO foodEntryDTO)
+  [HttpPut("{type}/{id}")]
+  public async Task<IActionResult> UpdateEntry([FromRoute] string type, [FromRoute] string id, [FromBody] JsonElement entryDTO)
   {
-    var foodEntry = await _foodEntryRepository.CreateFoodEntry(foodEntryDTO.Adapt<FoodEntry>());
-    return CreatedAtAction(nameof(GetFoodEntry), new { id = foodEntry.Id }, foodEntryDTO);
+    if (type == "food")
+    {
+      var foodEntryDTO = new UpdateFoodEntryDTO
+      {
+        FoodId = entryDTO.GetProperty("foodId").GetString(),
+        ServingSize = entryDTO.GetProperty("servingSize").GetInt32(),
+        ServingSizeUnit = entryDTO.GetProperty("servingSizeUnit").GetString(),
+        CreatedBy = entryDTO.GetProperty("createdBy").GetString(),
+      };
+      if (foodEntryDTO == null)
+        return BadRequest("Invalid data");
+      var foodEntry = foodEntryDTO.Adapt<FoodEntry>();
+      foodEntry.Id = id;
+      var updatedFoodEntry = await _foodEntryRepository.UpdateFoodEntry(id, foodEntry);
+      if (updatedFoodEntry == null)
+        return NotFound("Food entry not found");
+      return Ok(updatedFoodEntry.Adapt<FoodEntryDTO>());
+    }
+    else if (type == "meal")
+    {
+      var mealEntryDTO = new UpdateMealEntryDTO
+      {
+        MealId = entryDTO.GetProperty("mealId").GetString(),
+        CreatedBy = entryDTO.GetProperty("createdBy").GetString(),
+      };
+      if (mealEntryDTO == null)
+        return BadRequest("Invalid data");
+      var mealEntry = mealEntryDTO.Adapt<MealEntry>();
+      mealEntry.Id = id;
+      var updatedMealEntry = await _mealEntryRepository.UpdateMealEntry(id, mealEntry);
+      if (updatedMealEntry == null)
+        return NotFound("Meal entry not found");
+      return Ok(updatedMealEntry.Adapt<MealEntryDTO>());
+    }
+    return BadRequest("Invalid entry type");
   }
 
-  [HttpPost("meal")]
-  public async Task<IActionResult> CreateMealEntry([FromBody] CreateMealEntryDTO mealEntryDTO)
+  [HttpDelete("{type}/{id}")]
+  public async Task<IActionResult> DeleteEntry([FromRoute] string type, [FromRoute] string id)
   {
-    var mealEntry = await _mealEntryRepository.CreateMealEntry(mealEntryDTO.Adapt<MealEntry>());
-    return CreatedAtAction(nameof(GetMealEntry), new { id = mealEntry.Id }, mealEntryDTO);
+    if (type == "food")
+    {
+      var deletedFoodEntry = await _foodEntryRepository.DeleteFoodEntry(id);
+      if (deletedFoodEntry == null)
+        return NotFound("Food entry not found");
+      return Ok(deletedFoodEntry.Adapt<FoodEntryDTO>());
+    }
+    else if (type == "meal")
+    {
+      var deletedMealEntry = await _mealEntryRepository.DeleteMealEntry(id);
+      if (deletedMealEntry == null)
+        return NotFound("Meal entry not found");
+      return Ok(deletedMealEntry.Adapt<MealEntryDTO>());
+    }
+    return BadRequest("Invalid entry type");
   }
-
-  [HttpPut("food/{id}")]
-  public async Task<IActionResult> UpdateFoodEntry([FromRoute] string id, [FromBody] UpdateFoodEntryDTO foodEntryDTO)
-  {
-    var foodEntry = foodEntryDTO.Adapt<FoodEntry>();
-    foodEntry.Id = id;
-    var updatedFoodEntry = await _foodEntryRepository.UpdateFoodEntry(id, foodEntry);
-
-    if (updatedFoodEntry == null)
-      return NotFound("Food entry not found");
-
-    return Ok(updatedFoodEntry.Adapt<FoodEntryDTO>());
-  }
-
-  [HttpPut("meal/{id}")]
-  public async Task<IActionResult> UpdateMealEntry([FromRoute] string id, [FromBody] UpdateMealEntryDTO mealEntryDTO)
-  {
-    var mealEntry = mealEntryDTO.Adapt<MealEntry>();
-    mealEntry.Id = id;
-    var updatedMealEntry = await _mealEntryRepository.UpdateMealEntry(id, mealEntry);
-
-    if (updatedMealEntry == null)
-      return NotFound("Meal entry not found");
-
-    return Ok(updatedMealEntry.Adapt<MealEntryDTO>());
-  }
-
-  [HttpDelete("food/{id}")]
-  public async Task<IActionResult> DeleteFoodEntry([FromRoute] string id)
-  {
-    var deletedFoodEntry = await _foodEntryRepository.DeleteFoodEntry(id);
-    
-    if (deletedFoodEntry == null)
-      return NotFound("Food entry not found");
-
-    return Ok(deletedFoodEntry.Adapt<FoodEntryDTO>());
-  }
-
-  [HttpDelete("meal/{id}")]
-  public async Task<IActionResult> DeleteMealEntry([FromRoute] string id)
-  {
-    var deletedMealEntry = await _mealEntryRepository.DeleteMealEntry(id);
-
-    if (deletedMealEntry == null)
-      return NotFound("Meal entry not found");
-
-    return Ok(deletedMealEntry.Adapt<MealEntryDTO>());
-  }
-
 }
